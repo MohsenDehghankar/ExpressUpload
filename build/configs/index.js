69d52f7d5ea947efa36e022319047191e0ea2f63
m.dehghankar <mohsendehghankar@gmail.com>
2021-10-24 20:28:47 +0330

    update git ignore

--- build/configs/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Configs = void 0;
-exports.Configs = {
-    port: "8080",
-};
-//# sourceMappingURL=index.js.map
